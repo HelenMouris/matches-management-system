03c20361dadb2f096381db47d1f9218217d41b04
Mina Tamer <minatamer11@gmail.com>
2022-12-23 00:47:01 +0200

    stadium manager

---
+++ M3 DB/M3/M3/AddStadiumManager.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+
+
+namespace M3
+{
+    public partial class AddStadiumManager : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void addManager_Click(object sender, EventArgs e)
+        {
+            string connStr = WebConfigurationManager.ConnectionStrings["m2"].ToString();
+            SqlConnection conn = new SqlConnection(connStr);
+
+            String sname = name.Text;
+            String susername = username.Text;
+            String spassword = password.Text;
+            String stadiumname = stadium.Text;
+
+
+            SqlCommand addStadiumManagerProcedure = new SqlCommand("addStadiumManager", conn);
+            addStadiumManagerProcedure.CommandType = CommandType.StoredProcedure;
+            addStadiumManagerProcedure.Parameters.Add(new SqlParameter("@name", sname));
+            addStadiumManagerProcedure.Parameters.Add(new SqlParameter("@username", susername));
+            addStadiumManagerProcedure.Parameters.Add(new SqlParameter("@password", spassword));
+            addStadiumManagerProcedure.Parameters.Add(new SqlParameter("@staduimname", stadiumname));
+
+
+
+
+            conn.Open();
+            addStadiumManagerProcedure.ExecuteNonQuery();
+            conn.Close();
+
+            Response.Redirect("StadiumManager.aspx");
+        }
+    }
+}
---
+++ M3 DB/M3/M3/ViewRequests.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ViewRequests.aspx.cs" Inherits="M3.ViewRequests" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server" Width="849px">
+            </asp:GridView>
+        </div>
+
+    </form>
+</body>
+</html>
