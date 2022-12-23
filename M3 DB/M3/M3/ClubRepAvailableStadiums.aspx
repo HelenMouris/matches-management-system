7acc2515bb1f3cb90a8ab640ab639fdf93972ee7
helenmouris <helenmouris3@gmail.com>
2022-12-23 02:28:18 +0200

    add club representative available stadiums

---
+++ M3 DB/M3/M3/ClubRepAvailableStadiums.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ClubRepAvailableStadiums.aspx.cs" Inherits="M3.ClubRepAvailableStadiums" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <div>
+                <asp:Label ID="dateLabel" runat="server" Text="Start Date"></asp:Label>
+            </div>
+            <div>
+                <asp:TextBox ID="date" runat="server"></asp:TextBox>
+            </div>
+            <div>
+                <asp:Button ID="search" runat="server" Text="Search" OnClick="searchStadiums" />
+            </div>
+            <div>
+                <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ M3 DB/M3/M3/ClubRepAvailableStadiums.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace M3
+{
+    public partial class ClubRepAvailableStadiums : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void searchStadiums(object sender, EventArgs e)
+        {
+
+            string connStr = WebConfigurationManager.ConnectionStrings["m2"].ToString();
+            SqlConnection conn = new SqlConnection(connStr);
+
+            string startDate = date.Text; 
+
+            conn.Open();
+            var sql = String.Format("select * from viewAvailableStadiumsOn(@datetime)");
+            SqlCommand availableStadiums = new SqlCommand(sql, conn);
+            availableStadiums.Parameters.Add(new SqlParameter("@datetime", startDate));
+            SqlDataReader rdr = availableStadiums.ExecuteReader(CommandBehavior.CloseConnection);
+            GridView1.DataSource = rdr;
+            GridView1.DataBind();
+            conn.Close();
+        }
+    }
+}
